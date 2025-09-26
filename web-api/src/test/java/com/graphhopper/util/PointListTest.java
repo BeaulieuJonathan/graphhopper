39679981ca63c99e37af064aa0b1d603c9802268
Jonathan Beaulieu <jonathan.beaulieu.4@umontreal.ca>
2025-09-26 13:54:07 -0400

    Ajouté le fichier PointListTest.java pour les tests

---
+++ web-api/src/test/java/com/graphhopper/util/PointListTest.java
@@ -0,0 +1,16 @@
+package com.graphhopper.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+public class PointListTest {
+    
+
+    @Test
+    public void testAddPoints() {
+
+    }
+    
+}
