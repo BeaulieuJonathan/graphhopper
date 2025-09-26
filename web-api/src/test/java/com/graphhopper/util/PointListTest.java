8009ab6fd1aae4e4aef4ef82ec3ff0d283c24065
Jonathan Beaulieu <jonathan.beaulieu.4@umontreal.ca>
2025-09-26 14:17:12 -0400

    fixed duped pluggin and added PiTest to web-api module

--- web-api/src/test/java/com/graphhopper/util/PointListTest.java
+++ web-api/src/test/java/com/graphhopper/util/PointListTest.java
@@ -12,5 +12,15 @@ public class PointListTest {
     public void testAddPoints() {
 
     }
+
+    @Test
+    public void testEquals() {
+        assertEquals(Helper.createPointList(), PointList.EMPTY);
+        PointList list1 = Helper.createPointList(38.5, -120.2, 43.252, -126.453, 40.7, -120.95,
+                50.3139, 10.612793, 50.04303, 9.497681);
+        PointList list2 = Helper.createPointList(38.5, -120.2, 43.252, -126.453, 40.7, -120.95,
+                50.3139, 10.612793, 50.04303, 9.497681);
+        assertEquals(list1, list2);
+    }
     
 }
