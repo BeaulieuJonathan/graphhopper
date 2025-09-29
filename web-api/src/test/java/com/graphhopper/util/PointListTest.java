0c6d5e611d98b63bc7a89fa1a607256b1afb3e71
Jonathan Beaulieu <jonathan.beaulieu.4@umontreal.ca>
2025-09-29 17:13:14 -0400

    Added tests for EMptyList and Adding points in list

--- web-api/src/test/java/com/graphhopper/util/PointListTest.java
+++ web-api/src/test/java/com/graphhopper/util/PointListTest.java
@@ -7,20 +7,42 @@ import org.junit.jupiter.api.Test;
 
 public class PointListTest {
     
-
+    /* Ce test a pour objectif de vérifier qu'un objet PointList vide
+     * Est équivalent à PointList.EMPTY qu'elle soit 2D ou 3D.
+     */
     @Test
-    public void testAddPoints() {
+    public void test_EmptyList() {
+        PointList Empty2DPointList = new PointList(10,false);
+        PointList Empty3DPointList = new PointList(10,true);
 
-    }
+        assertEquals(Empty2DPointList,PointList.EMPTY);
 
-    @Test
-    public void testEquals() {
-        assertEquals(Helper.createPointList(), PointList.EMPTY);
-        PointList list1 = Helper.createPointList(38.5, -120.2, 43.252, -126.453, 40.7, -120.95,
-                50.3139, 10.612793, 50.04303, 9.497681);
-        PointList list2 = Helper.createPointList(38.5, -120.2, 43.252, -126.453, 40.7, -120.95,
-                50.3139, 10.612793, 50.04303, 9.497681);
-        assertEquals(list1, list2);
+        assertEquals(Empty3DPointList,PointList.EMPTY);
     }
     
+
+    /* Ce test a pour objectif de vérifier l'ajout de points dans la liste */
+    @Test
+    public void test_addition() {
+        PointList Liste = new PointList();
+
+        Liste.add(1, 1);
+        Liste.add(2, 2);
+        Liste.add(3, 3);
+        Liste.add(4, 4);
+        Liste.add(5, 5);
+
+        // ajout de points par une list
+        PointList ajouterPoints = new PointList();
+        ajouterPoints.add(8,8);
+        ajouterPoints.add(9,99);
+        ajouterPoints.add(10,10);
+
+        Liste.add(ajouterPoints);
+
+        assertEquals(8,Liste.size());
+        assertEquals(9, Liste.getLat(6));
+        assertEquals(99, Liste.getLon(6));
+    }
+
 }
