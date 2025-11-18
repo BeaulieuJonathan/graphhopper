c6990d28693364f23f81af7865b8d4a652f4e5fb
joeudem <joqcool@gmail.com>
2025-11-18 00:51:32 -0500

    Add 2 tests in GHResponseTest and created ResponsePathTest.

---
+++ web-api/src/test/java/com/graphhopper/util/ResponsePathTest.java
@@ -0,0 +1,34 @@
+package com.graphhopper.util;
+
+import com.graphhopper.ResponsePath;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ResponsePathTest {
+
+    @Test
+    public void testHasErrorsWithMockedThrowable() {
+        Throwable t = mock(Throwable.class);
+
+        ResponsePath path = new ResponsePath();
+        path.addError(t);
+
+        assertTrue(path.hasErrors(),
+                "ResponsePath should report an error when a mocked Throwable is added");
+        assertEquals(1, path.getErrors().size());
+        assertSame(t, path.getErrors().get(0));
+    }
+
+    @Test
+    public void testGetDebugInfoWithMockedString() {
+        ResponsePath path = new ResponsePath();
+
+        path.addDebugInfo("init-debug");
+        path.addDebugInfo("calc-debug");
+
+        String result = path.getDebugInfo();
+
+        assertEquals("init-debug;calc-debug", result);
+    }
+}
